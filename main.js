d7546b0014ec4aa319c8326ab076b92ce8bb9161
Anju0307 <jropero111@gmail.com>
2022-09-30 22:48:01 -0500

    punto 2

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ addEventListener("DOMContentLoaded", (e) => {
     let formulario = document.getElementById('formulario');
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
-        let numero = parseInt(prompt("Digite un numero entero positivo: "));
-        for(let i = 0; i <= 12; i++){
-            document.write(`${numero} X ${i} = ${numero * i} <br>`);
-        }
-        
+        let f1 = 1;
+        let f2 = 0;
+
+        for(let i = 0; i <= 10; i++){
+            f2 = f1 + 4;
+            f1 = f2 - 2;
+            document.write(`${f2} <br>`);
+            document.write(`${f1} <br>`);          
+        }          
     })
 })
