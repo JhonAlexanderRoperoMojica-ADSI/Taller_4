f9d2715d9171e8c02769c34f502026caa1505d0c
Anju0307 <jropero111@gmail.com>
2022-10-01 18:03:06 -0500

    punto 6

--- main.js
+++ main.js
@@ -2,31 +2,32 @@ addEventListener("DOMContentLoaded", (e) => {
     let formulario = document.getElementById('formulario');
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
-        let suma = 0;
-        let A = 0;
-        let B = 0;
-        let C = 0;
-        
-        let ventas = document.getElementById("ventas").value;
+        let empleado = document.getElementById("empleado").value;
+        for (let i = 0; i < empleado; i++) {
+            let nombre = prompt(`Ingrese el nombre del empelado ${i}`);
+            let salirio = parseFloat(prompt(`Ingrese el salario del empleado ${nombre}`));
+            let tiempo = parseInt(prompt(`Ingrese el tiempo trabajdo en la pempresa del empleado ${nombre}`));
 
-        for(let i = 0; i <= ventas; i++){
-            let Valor = parseFloat(prompt(`Ingrese el precio de la primera venta: ${i}`));
-
-            if(Valor > 1000){
-                A = A +1;
+            if (tiempo >= 1 && tiempo < 5) {
+                let total = salirio + 100;
+                document.getElementById("serie").innerHTML = `El trabajador: ${nombre} ,Tendra un aumento de 100 mil pesos`;
+                document.getElementById("serie1").innerHTML = `El salario total es de: ${total} , mil pesos`;
             }
-            else if (1000 >= Valor && Valor > 500){
-                B = B + 1;    
+            else if (tiempo >= 5 && tiempo < 10) {
+                let total = salirio + 250;
+                document.getElementById("serie").innerHTML = `El trabajador: ${nombre} ,Tendra un aumento de 250 mil pesos`;
+                document.getElementById("serie1").innerHTML = `El salario total es de: ${total} , mil pesos`;
             }
-            else{
-                C = C + 1;
+            else if(tiempo >= 10 && tiempo < 20){
+                let total = salirio + 400;
+                document.getElementById("serie").innerHTML = `El trabajador: ${nombre} ,Tendra un aumento de 400 mil pesos`;
+                document.getElementById("serie1").innerHTML = `El salario total es de: ${total} , mil pesos`;
             }
-            suma += Valor;
-
-            document.getElementById("serie").innerHTML = `Ventas Mayores a 1000 son: ${A} `;
-            document.getElementById("serie1").innerHTML = `Ventas Mayores a 500 menosres o iguales a 1000 son: ${B} `;
-            document.getElementById("serie2").innerHTML = `Ventas Menores o iguales a 500 son: ${C} `;
-            document.getElementById("serie3").innerHTML = `Las Ventas totales son: ${suma} `;
+            else if (tiempo >= 20){
+                let total = salirio + 550;
+                document.getElementById("serie").innerHTML = `El trabajador: ${nombre} ,Tendra un aumento de 550 mil pesos`;
+                document.getElementById("serie1").innerHTML = `El salario total es de: ${total} , mil pesos`;
+            }   
         }
     })
 })
