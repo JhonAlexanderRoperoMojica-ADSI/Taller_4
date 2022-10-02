29ed29877dbc8d850f04e2abc02e78df0d5f0af6
Anju0307 <jropero111@gmail.com>
2022-10-01 19:40:17 -0500

    punto 10

--- main.js
+++ main.js
@@ -2,25 +2,14 @@ addEventListener("DOMContentLoaded", (e) => {
     let formulario = document.getElementById('formulario');
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
-        let mayor = 500000;
-        const iva1 = 0.19;
-        let cant = parseFloat(prompt("ingrese la cantida de articulos"));
-        for (i = 1; i <= cant; i++) {
-            let valor = parseInt(prompt(`Ingrese el valor del árticulo: ${i}`));
-            let unidad = parseInt(prompt(`Unidades compradas:`));
 
-            let sub_tot = valor * unidad;
-            let iva2 = sub_tot * iva1;
-            let tot_pag = sub_tot + iva2;
+        let num = parseFloat(prompt("ingrese el valor a calvular"));
+        for (i = 1; i <= num; i++) {
+            let total1 = i ** 2;
+            let total2 = i ** i;
 
-            if (tot_pag > mayor) {
-                let des = tot_pag * 0.15;
-                let desc_tot = tot_pag - des;
-                document.getElementById("serie").innerHTML = `Su compra supero los 500.000 , El pago con IVA es: ${tot_pag} , El total a pagar es: ${desc_tot}`;
-            }
-            else {
-                document.getElementById("serie").innerHTML = `El subtotal es: ${sub_tot} , El IVA es: ${iva1} , El total a pagar es ${tot_pag}`
-            }
+            document.getElementById("serie").innerHTML = `el primer resultado es: ${total1}`;
+            document.getElementById("serie1").innerHTML = `el segundo resultado es: ${total2}`;
         }
     })
 })
